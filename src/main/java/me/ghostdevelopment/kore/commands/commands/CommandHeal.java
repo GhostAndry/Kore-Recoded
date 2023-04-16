45bee749509c593ca93fe7d6c45c0c037f3b7cd9
GhostAndry <andrearonzoni20062022@gmail.com>
2023-04-16 18:44:39 +0200

    .

--- src/main/java/me/ghostdevelopment/kore/commands/commands/CommandHeal.java
+++ src/main/java/me/ghostdevelopment/kore/commands/commands/CommandHeal.java
@@ -47,7 +47,6 @@ public class CommandHeal extends Command {
                     return;
                 }
             }
-
         }else{
             if(!(args.length==1)){
                 sender.sendMessage(Utils.Color(LangFile.getFile().getString("heal.usage.console")
