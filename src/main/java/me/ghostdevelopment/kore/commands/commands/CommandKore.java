eabf2687a76b7e6d9434b022a7e060cde0f80363
GhostAndry <andrearonzoni20062022@gmail.com>
2023-04-18 14:09:53 +0200

    updated /kore help

--- src/main/java/me/ghostdevelopment/kore/commands/commands/CommandKore.java
+++ src/main/java/me/ghostdevelopment/kore/commands/commands/CommandKore.java
@@ -53,11 +53,11 @@ public class CommandKore extends Command {
                             "&7/gms &1[player]\n" +
                             "&7/gma &1[player]\n" +
                             "&7/gmsp &1[player]\n" +
-                            "&7/fly &1[player]" +
-                            "&7/teleport /tp &c<player> &1[player]" +
-                            "&7/heal &1[player]" +
-                            "&7/god &1[player]" +
-                            "&7/vanish /v &1[player]" +
+                            "&7/fly &1[player]\n" +
+                            "&7/teleport /tp &c<player> &1[player]\n" +
+                            "&7/heal &1[player]\n" +
+                            "&7/god &1[player]\n" +
+                            "&7/vanish /v &1[player]\n" +
                             "\n"
                     ));
                 }else{
