47667762817574add9b552e788ead61120504bec
GhostAndry <andrearonzoni20062022@gmail.com>
2023-06-25 17:27:09 +0200

    2023-06-25 17:27

--- upload.bat
+++ upload.bat
@@ -1,6 +1,5 @@
 @echo off
 
-REM Ottenere la data e l'ora attuali
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
     set "day=%%a"
     set "month=%%b"
@@ -13,14 +12,11 @@ for /f "tokens=1-3 delims=: " %%a in ('time /t') do (
     set "second=%%c"
 )
 
-REM Formattare la data e l'ora
 set "formatted_date=%year%-%month%-%day%"
 set "formatted_time=%hour%:%minute%"
 
-REM Eseguire il commit con il messaggio contenente la data e l'ora
 git commit -a -m "%formatted_date% %formatted_time%"
-rem git commit -a -m "Update to 1.8"
+
 git push
 
-REM Visualizzare l'output del commit
 echo Commit eseguito con successo.
