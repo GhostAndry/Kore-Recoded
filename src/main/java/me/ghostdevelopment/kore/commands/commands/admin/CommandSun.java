e5b698e65d84d2a19fcea6e4e974df3a3413da74
GhostAndry <104942440+GhostAndry@users.noreply.github.com>
2023-10-06 18:21:40 +0200

    test

---
+++ src/main/java/me/ghostdevelopment/kore/commands/commands/admin/CommandSun.java
@@ -0,0 +1,23 @@
+package me.ghostdevelopment.kore.commands.commands.admin;
+
+import me.ghostdevelopment.kore.commands.CommandInfo;
+import me.ghostdevelopment.kore.commands.KoreCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+
+@CommandInfo(name = "sun", permission = "kore.sun")
+public class CommandSun extends KoreCommand {
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        for(World world : Bukkit.getServer().getWorlds()){
+            world.setTime(1000);
+            world.setStorm(false);
+            world.setWeatherDuration(1000000000);
+        }
+
+    }
+
+}
